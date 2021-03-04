5f85917b3b35cdfdd7f105679e7f740b3aafa197
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-04 23:59:04 +0300

    [Sequences - Lists - Sets]

--- Lecture-2/sequences.py
+++ Lecture-2/sequences.py
@@ -5,10 +5,11 @@ coordinateX = 10.0
 coordinateY = 20.0
 
 """
-    list => sequence of mutable values
-    tuple => sequence of immutable values
-    set => collection of unique values
-    dict => collection of key-value pairs
-    
+    Date Structures
+        list => sequence of mutable values
+        tuple => sequence of immutable values
+        set => collection of unique values
+        dict => collection of key-value pairs
+
 """
 coordinate = (coordinateX, coordinateY) # (10.0, 20.0)
