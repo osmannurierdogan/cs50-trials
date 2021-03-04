561b80b2ceb05c62bbf49e7a6dc3827a850aeb0b
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-05 00:43:33 +0300

    Class files added.

---
+++ Lecture-2/classes.py
@@ -0,0 +1,9 @@
+class Point():
+    def __init__(self, coordinateX, coordinateY): # Constructor !!! TODO 
+        self.x = coordinateX
+        self.y = coordinateY
+
+p = Point(15,5)
+
+print(p.x)
+print(p.y)
