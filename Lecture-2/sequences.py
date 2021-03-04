2aa316b294894f0efa6a444296a924a1bfcd22eb
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-04 23:49:50 +0300

    More Python Files Added.

---
+++ Lecture-2/sequences.py
@@ -0,0 +1,14 @@
+name = ['Henry', 'Ron', 'Hermione']
+print(name[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+"""
+    list => sequence of mutable values
+    tuple => sequence of immutable values
+    set => collection of unique values
+    dict => collection of key-value pairs
+    
+"""
+coordinate = (coordinateX, coordinateY) # (10.0, 20.0)
