d50aa90ef629e411facd30ec4b712aff49f51caf
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-05 00:09:11 +0300

    [Loops - Dictionaries File Added.]

---
+++ Lecture-2/dictionaries.py
@@ -0,0 +1,6 @@
+houses = {"Harry" : "Gryffindor", "Draco" : "Slytherin"}
+
+print("Harry's House => ", houses["Harry"])
+
+houses["Hermione"] = 'Gryffindor'
+print("Hermione's House => ", houses['Hermione'])
