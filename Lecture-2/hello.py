5c76486edab41218af5610d510069ac14d151355
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-04 12:45:31 +0300

    Python file is updated.

--- Lecture-2/hello.py
+++ Lecture-2/hello.py
@@ -1,4 +1,10 @@
 print("Hello World!")
+name = input("Name : ")
+surname = input("Surname : ")
+print(f"Hello, {name} {surname}".format(name = name, surname = surname))
 
+""" 
 for i in range(5):
-    print(i * '*')
+    print(i * '*') 
+
+"""
