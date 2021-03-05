56af4d28ed63dadcd1acfb77e9d49432e1e3aa1d
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-05 15:49:19 +0300

    Some syntax errors fixed.

--- Lecture-3/lecture3/lecture3/urls.py
+++ Lecture-3/lecture3/lecture3/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("hello.urls")),
-    path('newyear/', include("newyears.urls"))
+    path('newyear/', include("newyear.urls"))
 ]
--- Lecture-3/lecture3/newyear/views.py
+++ Lecture-3/lecture3/newyear/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import datetime
+from django.http import HttpResponse
 
 # Create your views here.
 
