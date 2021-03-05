d45ed7a618733aaf53d87dc35732abe5cc69af1d
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-05 12:53:32 +0300

    More Django files added.

--- Lecture-3/lecture3/hello/views.py
+++ Lecture-3/lecture3/hello/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name): # eski versiyon return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
