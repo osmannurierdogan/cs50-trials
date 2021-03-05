61edef2e189d2a4e61c543df54da2e88bf4ad79c
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-05 19:32:13 +0300

    Static CSS File Added.

--- Lecture-3/lecture3/newyear/views.py
+++ Lecture-3/lecture3/newyear/views.py
@@ -8,5 +8,5 @@ from django.http import HttpResponse
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html",{
-        "newyear": now.month == 1 and now.day == 1
+        "newyear": now.month == 1 and now.day == 1 # True yazarsak => Yes elde ederiz
     })
