b0249c7378605d838b4a2e7d2e79c9969b8259ea
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-06 23:17:00 +0300

    Tasks App Files Added.

--- Lecture-3/http.py
+++
@@ -1,9 +0,0 @@
-"""
-Status Code             Description
-
-    200        =>       OK
-    301        =>       Moved Permanently
-    403        =>       Forbidden
-    404        =>       Not Found
-    500        =>       Internal Server Error
-"""
