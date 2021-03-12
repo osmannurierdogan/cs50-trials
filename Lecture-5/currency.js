dc4e3cebf907aac182ea21cf30fc5b3e2bb848aa
Osman Nuri Erdogan <osmerd04@gmail.com>
2021-03-12 08:15:59 +0300

    Dinamik olarak exchange converter yapmaya calisiyorum.

--- Lecture-5/currency.js
+++ Lecture-5/currency.js
@@ -3,7 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('https://api.exchangeratesapi.io/latest?base=USD')
         .then(response => response.json()) // return response.json()
         .then(data => {
-            const currencyType = document.querySelectorAll('.currency').value;
+            const currencyNodeArray = document.querySelectorAll('.currencyType');
+            const currencyArray = [...currencyNodeArray];
+            //const currencyType = currencyArray[0];
+            currencyArray.forEach(currencyType => {
+                return currencyType = currencyArray[currencyType];
+            });
             const currencyBalance = document.querySelector('#currency').value;
             const rate = data.rates[currencyType];
             const balance = currencyBalance * rate;
